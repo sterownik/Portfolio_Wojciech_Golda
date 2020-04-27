13b0a4a56960c1879a0ac4c04b616c6b229aa436
Wojtek <4golda4@gmail.com>
2020-04-27 18:26:30 +0200

    Dodanie kolejnej sekcji związanej z umiętnościami

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
 var i = 0;
+var licznikikon = 0;
 var txt = 'Witam, nazywam się Wojciech Gołda i oto moje porfolio.';
 var speed = 75;
 typeWriter();
+var icons = ["fa-android", "fa-html5", "fa-css3-alt", "fa-js", "fa-java", "fa-git-square", "fa-react", "fa-node-js"];
+var liczbaumiejetnosci = icons.length;
+$(".numer").text(licznikikon + 1 + " / " + liczbaumiejetnosci);
+$(".fa-arrow-right").on("click", function () {
+
+    $(".fab").removeClass(icons[licznikikon]);
+
+    licznikikon++;
+    $(".fa-arrow-right").animate({ right: '0' }, "slow");
+    $(".fa-arrow-right").animate({ right: '5%' }, "slow");
+
+
+    if (licznikikon == icons.length) {
+        licznikikon = 0;
+    }
+    $(".numer").text(licznikikon + 1 + " / " + liczbaumiejetnosci);
+    console.log(licznikikon);
+    $(".fab").animate({ opacity: '0' }, "slow");
+
+
+    $(".fab").addClass(icons[licznikikon]);
+
+    $(".fab").animate({ opacity: '1' }, "slow");
+
+
+})
+$(".fa-arrow-left").on("click", function () {
+    $(".fab").removeClass(icons[licznikikon]);
+    licznikikon--;
+    $(".fa-arrow-left").animate({ left: '0' }, "slow");
+    $(".fa-arrow-left").animate({ left: '5%' }, "slow");
+
+    if (licznikikon < 0) {
+        licznikikon = icons.length - 1;
+    }
+    $(".numer").text(licznikikon + 1 + " / " + liczbaumiejetnosci)
+    console.log(licznikikon);
+    $(".fab").animate({ opacity: '0' }, "slow");
+    $(".fab").addClass(icons[licznikikon]);
+    $(".fab").animate({ opacity: '1' }, "slow");
+})
 
 function typeWriter() {
     if (i < txt.length) {
@@ -17,7 +59,7 @@ $(document).on("scroll", function () {
     }
 })
 
-$(".fas").on("click", function () {
+$(".fa-arrow-down").on("click", function () {
     console.log("kliknieto");
     //robi odrazu id
 
